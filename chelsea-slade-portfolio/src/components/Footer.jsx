cc6aeaa7223a4276a1252169c7ff4d143fa409ba
Chelsea Slade <chelsea.mayne@keyin.com>
2025-05-10 08:48:23 -0230

    Footer update

--- chelsea-slade-portfolio/src/components/Footer.jsx
+++ chelsea-slade-portfolio/src/components/Footer.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import github from "../pics/github.svg"
+import linkedin from "../pics/linkedin.svg"
 
 const Footer = () => {
   return (
     <>
       <div className="footer">
         <footer>
-          <p>Socials Here</p>
+          <a href="https://github.com/chelseaslade"><img src={github} alt="GitHub" className="socialIcon" /></a>
+          <a href="www.linkedin.com/in/chelsea-slade-601a352a0"><img src={linkedin} alt="LinkedIn" className="socialIcon" /></a>
         </footer>
       </div>
     </>
