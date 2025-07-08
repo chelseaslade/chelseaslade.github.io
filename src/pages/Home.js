c8a5d0b8c851748fe737741f8d2a0b911fe3b965
Chelsea Slade <chelsea.mayne@keyin.com>
2025-07-08 19:15:31 -0230

    View resume PDF link functional

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import profilepic from '../pics/chelseaslade1.JPG';
+import pdf from '../assets/cslade_resume_PDF.pdf';
 
 const Home = () => {
     return (
@@ -16,8 +17,8 @@ const Home = () => {
         <h2>Junior Software Developer</h2>
         <h4>Java - SQL Databases - Javascript - React</h4>
         <p>Hello! I'm Chelsea. Thanks for checking out my portfolio. </p> 
-        <button class="genButton">View Resume</button>
-        <Link to="/about"><button class="genButton">About Me</button></Link>
+        <a href={pdf} target='_blank' rel="noopener noreferrer"><button className="genButton">View Resume</button></a>
+        <Link to="/about"><button className="genButton">About Me</button></Link>
         </div>
         </div>
         </div>
