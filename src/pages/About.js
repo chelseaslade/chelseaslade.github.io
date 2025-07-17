7c18925d1c8f8d3bd815d55473610b100080a42f
Chelsea Slade <chelsea.mayne@keyin.com>
2025-07-17 13:12:42 -0230

    Added badge images to about me page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import cloudArchitect from "../pics/cloudArchitect.png";
+import cloudDevelop from "../pics/cloudDevelop.png";
+import cloudFoundations from "../pics/cloudFoundations.png";
 
 const About = () => {
     return (
@@ -10,16 +12,20 @@ const About = () => {
                     <p>Hello! My name is Chelsea Slade, and I'm a Software Developer. </p>
                     <h4>Skills</h4>
                     <ul>
-                        <li>HTML</li>
-                        <li>CSS</li>
+                        <li>Java</li>
+                        <li>HTML & CSS</li>
                         <li>JavaScript</li>
                         <li>React</li>
                     </ul>
-                <p>I'm passionate about many things, including but not limited to programming, design & illustration,
-                biological science, and literature. I have a Bachelor of Science in Biochemistry (Nutrition) from Memorial University of
-                Newfoundland and Labrador, and frequently indulge in free online skills classes. In my free time, I
-                enjoy walks with my husband and our energetic pup Suki along the coast of sunny Conception Bay South,
-                and painting colourful birds and buildings that remind me of home.</p>
+                    <div className="certList">
+                    <h4>Certifications</h4>
+                    <ul>
+                        <li><img src={cloudArchitect} alt="AWS Cloud Architect Badge" class="awsBadge" /></li>
+                        <li><img src={cloudDevelop} alt="AWS Cloud Development Badge" class="awsBadge" /></li>
+                        <li><img src={cloudFoundations} alt="AWS Cloud Foundations Badge" class="awsBadge" /></li>
+                    </ul>
+                    </div>
+                <p></p>
                 <p>I look forward to a continued career in the amazing technology industry here in Newfoundland and
                 Labrador!</p>
                 </div>
