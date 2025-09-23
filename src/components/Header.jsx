8cb3fdfdafdacfcc2374dcba25d9d2a227bac1b5
Chelsea Slade <chelsea.mayne@keyin.com>
2025-09-23 14:41:53 -0230

    Lined up contact button in header & made mobile friendly with hamburger menu on small screen width

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,32 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const Header = () => {
-    return (
-      <>
-        <div className="header">
-          <header className="banner">
-              <nav className="nav">
-                <ul className="navList">
-                  <li><Link to="/">Home</Link></li>
-                  <li><Link to="/projects">Projects</Link></li>
-                  <li><Link to="/about">About Me</Link></li>
-                  <li><Link to="/contact"><button id="contactButton">Contact</button></Link></li>
-                </ul>
-              </nav>
-          </header>
-        </div>
-      </>
-    );
+
+  const [isOpen, setIsOpen] = useState(false);
+  
+  return (
+    <header className="header">
+      <nav className="nav">
+        <button
+          className="hamburger"
+          onClick={() => setIsOpen(!isOpen)}
+          aria-label="Toggle navigation"
+        >
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </button>
+
+        <ul className={`navList ${isOpen ? "open" : ""}`}>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/projects">Projects</Link></li>
+          <li><Link to="/about">About Me</Link></li>
+          <li><Link to="/contact" className="contactLink">Contact</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
   };
   
   export default Header;
