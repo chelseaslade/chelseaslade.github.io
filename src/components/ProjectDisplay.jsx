23c0ec09f5955f431296c02a324acfcc047034dd
Chelsea Slade <chelsea.mayne@keyin.com>
2025-11-08 20:33:53 -0330

    Added project images and did some tidying up

--- src/components/ProjectDisplay.jsx
+++ src/components/ProjectDisplay.jsx
@@ -5,11 +5,14 @@ const ProjectDisplay = ({ project }) => {
   window.open(project.link, "_blank", "noopener, noreferrer");
   };
 
+  const imageSrc = project.image ? require(`../${project.image}`) : null;
+
   return (
     <div className="projectDisplay">
       <div className="projectContainer">
       <h4>{project.title}</h4>
-      <img src={project.image} alt={project.title + " image"} className="projectImage" />
+      <div className="imageContainer">
+      {imageSrc && <img src={imageSrc} alt={project.title} className="projectImage" />}</div>
       <p>Tags: {project.tags.join(", ")}</p>
       <p>Description: {project.description}</p>
       <button onClick={handleClick} className="genButton" id="projectLink">View on GitHub</button>
